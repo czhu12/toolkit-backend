a1de5a4ebadc9e1478fde706daccfee06c020251
Chris Oliver <excid3@gmail.com>
2022-06-09 08:53:47 -0500

    Add Typescript annotation for TailwindCSS 3.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config */
 module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
