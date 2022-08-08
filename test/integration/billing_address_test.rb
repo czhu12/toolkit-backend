96dff5c15afc66d2a6e20aaee9470b32d741e731
Chris Oliver <excid3@gmail.com>
2022-08-08 12:02:26 -0500

    Billing address tests should stub the billing address enabled

--- test/integration/billing_address_test.rb
+++ test/integration/billing_address_test.rb
@@ -8,15 +8,39 @@ class BillingAddressTest < ActionDispatch::IntegrationTest
 
   test "can see section for adding/updating billing address on own account" do
     Jumpstart.config.stub(:payments_enabled?, true) do
-      sign_in @regular_user
-      get subscriptions_path
-      assert_select "h5#billing-address", I18n.t("subscriptions.billing_address.billing_address")
+      Jumpstart.config.stub(:collect_billing_address?, true) do
+        sign_in @regular_user
+        get subscriptions_path
+        assert_select "h5#billing-address", I18n.t("subscriptions.billing_address.billing_address")
+      end
     end
   end
 
   test "can add a billing address" do
     Jumpstart.config.stub(:payments_enabled?, true) do
-      assert_difference("Address.count", 1) do
+      Jumpstart.config.stub(:collect_billing_address?, true) do
+        assert_difference("Address.count", 1) do
+          sign_in @regular_user
+          post billing_address_url, params: {
+            address: {
+              address_type: :billing,
+              line1: "123 Test Dr",
+              line2: "Apt# 702",
+              city: "NewTestCity",
+              country: "US",
+              state: "TestState",
+              postal_code: "12345",
+              addressable: @regular_user.accounts.first
+            }
+          }
+        end
+      end
+    end
+  end
+
+  test "cannot add a billing address without postal_code and country" do
+    Jumpstart.config.stub(:payments_enabled?, true) do
+      Jumpstart.config.stub(:collect_billing_address?, true) do
         sign_in @regular_user
         post billing_address_url, params: {
           address: {
@@ -24,62 +48,48 @@ class BillingAddressTest < ActionDispatch::IntegrationTest
             line1: "123 Test Dr",
             line2: "Apt# 702",
             city: "NewTestCity",
-            country: "US",
             state: "TestState",
-            postal_code: "12345",
             addressable: @regular_user.accounts.first
           }
         }
+
+        assert_response :unprocessable_entity
       end
-    end
-  end
-
-  test "cannot add a billing address without postal_code and country" do
-    Jumpstart.config.stub(:payments_enabled?, true) do
-      sign_in @regular_user
-      post billing_address_url, params: {
-        address: {
-          address_type: :billing,
-          line1: "123 Test Dr",
-          line2: "Apt# 702",
-          city: "NewTestCity",
-          state: "TestState",
-          addressable: @regular_user.accounts.first
-        }
-      }
-
-      assert_response :unprocessable_entity
     end
   end
 
   test "cannot update a billing address without postal_code and country" do
     Jumpstart.config.stub(:payments_enabled?, true) do
-      sign_in @regular_user
-      post billing_address_url, params: {
-        address: {
-          address_type: :billing,
-          line1: "123 Test Dr",
-          line2: "Apt# 702",
-          city: "NewTestCity",
-          state: "TestState",
-          addressable: @regular_user.accounts.first
+      Jumpstart.config.stub(:collect_billing_address?, true) do
+        sign_in @regular_user
+        post billing_address_url, params: {
+          address: {
+            address_type: :billing,
+            line1: "123 Test Dr",
+            line2: "Apt# 702",
+            city: "NewTestCity",
+            state: "TestState",
+            addressable: @regular_user.accounts.first
+          }
         }
-      }
 
-      assert_response :unprocessable_entity
+        assert_response :unprocessable_entity
+      end
     end
   end
 
   test "can update a billing address" do
     Jumpstart.config.stub(:payments_enabled?, true) do
-      sign_in @updating_address_user
-      patch billing_address_url, params: {
-        address: {
-          line1: "456 Pickup Sticks Lane"
+      Jumpstart.config.stub(:collect_billing_address?, true) do
+        sign_in @updating_address_user
+        patch billing_address_url, params: {
+          address: {
+            line1: "456 Pickup Sticks Lane"
+          }
         }
-      }
 
-      assert_equal "456 Pickup Sticks Lane", @updating_address_user.personal_account.billing_address.line1
+        assert_equal "456 Pickup Sticks Lane", @updating_address_user.personal_account.billing_address.line1
+      end
     end
   end
 end
