1fd7c728fd640400f436680734f4aee41ca8cf13
Julian Rubisch <julian@julianrubisch.at>
2022-08-26 13:19:33 +0200

    feat: Extract Rolified concern
    and include in AccountUser and AccountInvitation

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -25,28 +25,13 @@ class AccountUser < ApplicationRecord
   # Do NOT to use any reserved words like `user` or `account`
   ROLES = [:admin, :member]
 
+  include Rolified
+
   belongs_to :account
   belongs_to :user
 
   validates :user_id, uniqueness: {scope: :account_id}
   validate :owner_must_be_admin, on: :update, if: -> { admin_changed? && account_owner? }
-
-  # Store the roles in the roles json column and cast to booleans
-  store_accessor :roles, *ROLES
-
-  # Cast roles to/from booleans
-  ROLES.each do |role|
-    scope role, -> { where("roles @> ?", {role => true}.to_json) }
-
-    define_method(:"#{role}=") { |value| super ActiveRecord::Type::Boolean.new.cast(value) }
-    define_method(:"#{role}?") { send(role) }
-  end
-
-  # You can use Postgres' jsonb operators to query the roles jsonb column
-  # https://www.postgresql.org/docs/current/functions-json.html
-  #
-  # Query where roles contains:
-  # scope :managers, -> { where("roles @> ?", {manager: true}.to_json) }
 
   def active_roles
     ROLES.select { |role| send(:"#{role}?") }.compact
