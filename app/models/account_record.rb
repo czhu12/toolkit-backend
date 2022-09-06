4b3af38123a3588530c05bfc82bae800b675d810
Chris Oliver <excid3@gmail.com>
2022-09-06 13:57:35 -0500

    Change AccountRecord to use ActsAsTenant

--- app/models/account_record.rb
+++ app/models/account_record.rb
@@ -1,20 +1,8 @@
 class AccountRecord < ApplicationRecord
+  # Inherit from this class to add ActsAsTenant multitenancy enforcement to a model.
+  # You can also add any account / multitenancy helpers here to be shared across your multitenant models.
+
   self.abstract_class = true
 
-  default_scope -> {
-    if ActsAsTenant.configuration.require_tenant && ActsAsTenant.current_tenant.nil? && !ActsAsTenant.unscoped?
-      raise ActsAsTenant::Errors::NoTenantSet
-    end
-
-    if ActsAsTenant.current_tenant
-      keys = [ActsAsTenant.current_tenant.send(pkey)]
-      keys.push(nil) if options[:has_global_records]
-
-      query_criteria = {fkey.to_sym => keys}
-      query_criteria[polymorphic_type.to_sym] = ActsAsTenant.current_tenant.class.to_s if options[:polymorphic]
-      where(query_criteria)
-    else
-      ActiveRecord::VERSION::MAJOR < 4 ? scoped : all
-    end
-  }
+  acts_as_tenant :account
 end
