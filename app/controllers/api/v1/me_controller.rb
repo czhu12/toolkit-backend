6faef9c946b792a8e214ecf012e59d633277607b
Chris Oliver <excid3@gmail.com>
2022-11-16 11:17:31 -0600

    Add DELETE /me endpoint for mobile app account deletion

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::MeController < Api::BaseController
   def show
     render partial: "users/user", locals: {user: current_user}
   end
+
+  def destroy
+    current_user.destroy
+    head :ok
+  end
 end
--- test/controllers/api/v1/me_controller_test.rb
+++ test/controllers/api/v1/me_controller_test.rb
@@ -2,11 +2,20 @@ require "test_helper"
 
 class MeControllerTest < ActionDispatch::IntegrationTest
   test "returns current user details" do
-    user = users(:one)
-
     get api_v1_me_url, headers: {Authorization: "token #{user.api_tokens.first.token}"}
     assert_response :success
 
     assert_equal user.name, response.parsed_body["name"]
+  end
+
+  test "delete current user" do
+    assert_difference "User.count", -1 do
+      delete api_v1_me_url, headers: {Authorization: "token #{user.api_tokens.first.token}"}
+      assert_response :success
+    end
+  end
+
+  def user
+    @user ||= users(:one)
   end
 end
