79523fc0516f0c94900efdf6901847bddc77f974
Chris Oliver <excid3@gmail.com>
2022-11-21 17:44:47 -0600

    Upgrade to Pay v6

--- app/controllers/payment_methods/stripe_controller.rb
+++ app/controllers/payment_methods/stripe_controller.rb
@@ -4,13 +4,19 @@ class PaymentMethods::StripeController < ApplicationController
 
   def show
     if @setup_intent.status == "succeeded"
-      current_account.set_payment_processor(:stripe)
+      payment_processor = current_account.set_payment_processor(:stripe)
       pay_payment_method = Pay::Stripe::PaymentMethod.sync(@setup_intent.payment_method)
       pay_payment_method.make_default!
+
+      # if any past_due subscriptions exist, attempt to pay them
+      payment_processor.retry_past_due_subscriptions!
+
       redirect_to root_path, notice: t("payment_methods.create.updated")
     else
       redirect_to root_path, alert: t("something_went_wrong")
     end
+  rescue Pay::ActionRequired => e
+    redirect_to pay.payment_path(e.payment.id)
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_201915) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_232410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -221,7 +221,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_31_201915) do
     t.decimal "application_fee_percent", precision: 8, scale: 2
     t.jsonb "metadata"
     t.bigint "customer_id"
+    t.datetime "current_period_start"
+    t.datetime "current_period_end"
+    t.boolean "metered"
+    t.string "pause_behavior"
+    t.datetime "pause_starts_at"
+    t.datetime "pause_resumes_at"
     t.index ["customer_id", "processor_id"], name: "index_pay_subscriptions_on_customer_id_and_processor_id", unique: true
+    t.index ["metered"], name: "index_pay_subscriptions_on_metered"
+    t.index ["pause_starts_at"], name: "index_pay_subscriptions_on_pause_starts_at"
   end
 
   create_table "pay_webhooks", force: :cascade do |t|
