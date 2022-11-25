50298f2622154e489c114d5a6ad9adca579513e6
Chris Oliver <excid3@gmail.com>
2022-11-25 13:59:26 -0600

    Remove console.logs

--- app/javascript/administrate/controllers/array_field_controller.js
+++ app/javascript/administrate/controllers/array_field_controller.js
@@ -5,13 +5,11 @@ export default class extends Controller {
 
   add(e) {
     e.preventDefault()
-    console.log(e.target)
     e.target.insertAdjacentHTML('beforebegin', this.templateTarget.innerHTML)
   }
 
   remove(e) {
     e.preventDefault()
-    console.log(e.target.closest(".row"))
     e.target.closest(".row").remove()
   }
 }
