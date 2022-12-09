d4ce3a1182fa4440d4458f7a28d4584b9d6c41a2
Chris Oliver <excid3@gmail.com>
2022-12-09 15:55:51 -0600

    Turbo automatically sets itself on window

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,10 @@
 
 // Rails functionality
 import Rails from "@rails/ujs"
-import { Turbo } from "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 
 // Make accessible for Electron and Mobile adapters
 window.Rails = Rails
-window.Turbo = Turbo
 
 require("@rails/activestorage").start()
 import "@rails/actiontext"
