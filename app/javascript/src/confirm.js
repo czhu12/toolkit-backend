4bc37906a12b4a446f4a6c49abfcb54fded4f35e
Chris Oliver <excid3@gmail.com>
2023-01-15 18:47:43 -0600

    Add confirm modal with required confirm text support

--- app/javascript/src/confirm.js
+++ app/javascript/src/confirm.js
@@ -1,6 +1,13 @@
 // Custom TailwindCSS modals for confirm dialogs
 function insertConfirmModal(message, element, button) {
+  let confirmInput = ""
+  let confirmText = button.dataset.turboConfirmText
   let description = button.dataset.turboConfirmDescription || ""
+
+  if (confirmText) {
+    confirmInput = `<input type="text" class="mt-4 form-control" data-behavior="confirm-text" />`
+  }
+
   let content = `
     <div id="confirm-modal" class="z-50 animated fadeIn fixed top-0 left-0 w-full h-full table" style="background-color: rgba(0, 0, 0, 0.8);">
       <div class="table-cell align-middle">
@@ -9,9 +16,11 @@ function insertConfirmModal(message, element, button) {
           <h4>${message}</h4>
           ${description}
 
+          ${confirmInput}
+
           <div class="flex justify-end items-center flex-wrap mt-6">
             <button data-behavior="cancel" class="btn btn-light-gray mr-2">Cancel</button>
-            <button data-behavior="commit" class="btn btn-danger focus:outline-none">Confirm</button>
+            <button disabled data-behavior="commit" class="btn btn-danger focus:outline-none">Confirm</button>
           </div>
         </div>
       </div>
@@ -20,7 +29,19 @@ function insertConfirmModal(message, element, button) {
 
   document.body.insertAdjacentHTML('beforeend', content)
   document.activeElement.blur()
-  return document.getElementById("confirm-modal")
+
+  let modal = document.getElementById("confirm-modal")
+
+  // Disable commit button until the value matches confirmText
+  if (confirmText) {
+    let commitButton = modal.querySelector("[data-behavior='commit']")
+    commitButton.disabled = true
+    modal.querySelector("input[data-behavior='confirm-text']").addEventListener("input", (event) => {
+      commitButton.disabled = (event.target.value != confirmText)
+    })
+  }
+
+  return modal
 }
 
 Turbo.setConfirmMethod((message, element, button) => {
