d1acb97b98944b83fc831779337baf39efb82903
Chris Oliver <excid3@gmail.com>
2023-03-07 21:17:36 -0600

    Set default mailer URL protocol to https for production & staging. Closes #17

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,6 +4,7 @@ Rails.application.reloader.to_prepare do
 
   if Rails.env.production? || Rails.env.staging?
     ActionMailer::Base.default_url_options[:host] = Jumpstart.config.domain
+    ActionMailer::Base.default_url_options[:protocol] = "https"
     ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
   end
 end
