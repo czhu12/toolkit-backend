c6cc62d81e42cff8c564d6ff82d1236cbdeb1c68
Chris Oliver <excid3@gmail.com>
2023-05-05 09:59:18 -0500

    Support multipart/form-data payloads for inbound webhooks

--- app/controllers/inbound_webhooks/application_controller.rb
+++ app/controllers/inbound_webhooks/application_controller.rb
@@ -2,8 +2,12 @@ module InboundWebhooks
   class ApplicationController < ActionController::API
     private
 
+    # Returns the payload for the webhook as a String
+    #
+    # Some services use multipart/form-data to encapsulate their JSON payload
+    # For multipart/form-data we will let Rails parse and then dump the params as JSON
     def payload
-      @payload ||= request.body.read
+      @payload ||= request.form_data? ? request.request_parameters.to_json : request.raw_post
     end
   end
 end
