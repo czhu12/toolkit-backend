39ac72caa78e3e4fb90c7a226b1c2b17e57d3c3e
Joe Masilotti <joe@masilotti.com>
2023-06-20 10:29:20 -0700

    Fix Turbo Native authentication
    Turbo Native still needs a 401 when hitting unauthorized endpoints to
    know when to show the native authentication screen.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+class TurboFailureApp < Devise::FailureApp
+  include Turbo::Native::Navigation
+
+  def respond
+    turbo_native_app? ? http_auth : super
+  end
+end
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -292,10 +300,11 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+    # manager.intercept_401 = false
+    # manager.default_strategies(scope: :user).unshift :some_external_strategy
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
