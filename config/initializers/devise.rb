b0f4cc1519d459e128bce908bbb74b26b6ca2b6b
Chris Oliver <excid3@gmail.com>
2023-06-26 16:37:02 -0500

    Refactor Devise 401 for Turbo Native and add tests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,8 +3,9 @@
 class TurboFailureApp < Devise::FailureApp
   include Turbo::Native::Navigation
 
-  def respond
-    turbo_native_app? ? http_auth : super
+  # Turbo Native requests that require authentication should return 401s to trigger the login modal
+  def http_auth?
+    turbo_native_app? || super
   end
 end
 
