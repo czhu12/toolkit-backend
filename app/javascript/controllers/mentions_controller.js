bc03f1be3c10ab2f9ba885558176bcfe18e08d79
Chris Oliver <excid3@gmail.com>
2023-06-28 18:31:32 -0500

    Lazily lookup Trix editor in mentions controller. #711

--- app/javascript/controllers/mentions_controller.js
+++ app/javascript/controllers/mentions_controller.js
@@ -6,7 +6,6 @@ export default class extends Controller {
   static targets = [ "field" ]
 
   connect() {
-    this.editor = this.fieldTarget.editor
     this.initializeTribute()
   }
 
@@ -18,7 +17,7 @@ export default class extends Controller {
       menuShowMinLength: 1,
     })
     this.tribute.attach(this.fieldTarget)
-    this.fieldTarget.addEventListener('tribute-replaced', this.replaced)
+    this.fieldTarget.addEventListener('tribute-replaced', this.replaced.bind(this))
     this.tribute.range.pasteHtml = this._pasteHtml.bind(this)
   }
 
@@ -33,6 +32,7 @@ export default class extends Controller {
       .catch(error => callback([]))
   }
 
+  // Inserts ActionText Attachment when a Tribute entry is selected
   replaced(e) {
     let mention = e.detail.item.original
     let attachment = new Trix.Attachment({
@@ -43,6 +43,7 @@ export default class extends Controller {
     this.editor.insertString(" ")
   }
 
+  // Remove text user typed for the mention
   _pasteHtml(html, startPos, endPos) {
     let range = this.editor.getSelectedRange()
     let position = range[0]
@@ -50,5 +51,10 @@ export default class extends Controller {
 
     this.editor.setSelectedRange([position - length, position])
     this.editor.deleteInDirection("backward")
+  }
+
+  // Lazily lookup the Trix editor
+  get editor() {
+    return this.fieldTarget.editor
   }
 }
