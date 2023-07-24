7a8a38259da8a337d7eab2d06b333cab7972ae70
Chris Oliver <excid3@gmail.com>
2023-07-24 13:34:18 -0500

    Support localStorage for themes.
    You can switch the theme using buttons:
    <%= button_tag "Light", class: "btn btn-white", data: { action: "theme#light" } %>
    <%= button_tag "Dark", class: "btn btn-white", data: { action: "theme#dark" } %>
    <%= button_tag "System", class: "btn btn-white", data: { action: "theme#system" } %>
    
    By default, the theme will be saved to the user's profile. Setting the
    storage value to "localStorage" will save the theme in the browser's
    localStorage and set the theme on page load.

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -1,13 +1,22 @@
 // Switches dark/light theme based upon the preference
+//
+// Set storage value to "localStorage" for saving the preference in the browser
+// Otherwise, preference is saved on User profile in database
+//
+// For localStorage, your head tag should include the following JS to apply the dark class before paint
+// document.documentElement.classList.toggle("dark", localStorage.theme === 'dark' || (localStorage.theme !== "light" && window.matchMedia('(prefers-color-scheme: dark)').matches))
 
 import { Controller } from "@hotwired/stimulus"
+import { patch } from "@rails/request.js"
 
 export default class extends Controller {
   static values = {
-    preference: String
+    preference: String,
+    storage: { type: String, default: "user" }
   }
 
   connect() {
+    if (this.storageValue == "localStorage") this.preferenceValue = localStorage.theme
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", this.preferenceValueChanged.bind(this))
   }
 
@@ -21,5 +30,36 @@ export default class extends Controller {
 
   get systemInDarkMode() {
     return window.matchMedia('(prefers-color-scheme: dark)').matches
+  }
+
+  light() {
+    this.preferenceValue = "light"
+    this.save()
+  }
+
+  dark() {
+    this.preferenceValue = "dark"
+    this.save()
+  }
+
+  system() {
+    this.preferenceValue = ""
+    this.save()
+  }
+
+  // This isn't called on preferenceValueChanged callback because that is fired on load which would trigger a save every page load
+  save() {
+    this.storageValue == "localStorage" ? this.saveToLocalStorage() : this.saveToUser()
+  }
+
+  saveToLocalStorage() {
+    localStorage.theme = this.preferenceValue
+  }
+
+  saveToUser() {
+    patch("/users", {
+      body: {user: {"theme": this.preferenceValue }},
+      contentType: "application/json"}
+    )
   }
 }
