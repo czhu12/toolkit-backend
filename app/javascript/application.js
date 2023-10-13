b3c7b1df9e65a29da602099ee58ed82113dfda75
Chris Oliver <excid3@gmail.com>
2023-10-13 11:35:53 -0500

    Remove Rails UJS (#786)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,24 +1,9 @@
 /* eslint no-console:0 */
 
-// Rails functionality
-import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
-
-// Make accessible for Electron and Mobile adapters
-window.Rails = Rails
-
 require("@rails/activestorage").start()
-import "@rails/actiontext"
-
-// ActionCable Channels
-import "./channels"
-
-// Stimulus controllers
-import "./controllers"
-
-// Jumpstart Pro & other Functionality
-import "./src/**/*"
 require("local-time").start()
 
-// Start Rails UJS
-Rails.start()
+import "./channels"
+import "./controllers"
+import "./src/**/*"
