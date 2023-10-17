069082287c4864104f2f7386de6df0697766263e
William Kennedy <williamkennedyjnr@gmail.com>
2023-10-17 15:05:41 +0100

    update mentions control to accept URL as a value

--- app/javascript/controllers/mentions_controller.js
+++ app/javascript/controllers/mentions_controller.js
@@ -4,6 +4,9 @@ import Trix from "trix"
 
 export default class extends Controller {
   static targets = [ "field" ]
+  static values = {
+    url: { type: String, default: '/users/mentions.json'}
+  }
 
   connect() {
     this.initializeTribute()
@@ -13,7 +16,12 @@ export default class extends Controller {
     this.tribute = new Tribute({
       allowSpaces: true,
       lookup: 'name',
-      values: this.fetchUsers,
+      values: (text, callback) => {
+        fetch(`${this.urlValue}?query=${text}`)
+          .then(response => response.json())
+          .then(users => callback(users))
+
+      },
       menuShowMinLength: 1,
     })
     this.tribute.attach(this.fieldTarget)
@@ -25,16 +33,10 @@ export default class extends Controller {
     this.tribute.detach(this.fieldTarget)
   }
 
-  fetchUsers(text, callback) {
-    fetch(`/users/mentions.json?query=${text}`)
-      .then(response => response.json())
-      .then(users => callback(users))
-      .catch(error => callback([]))
-  }
-
   // Inserts ActionText Attachment when a Tribute entry is selected
   replaced(e) {
     let mention = e.detail.item.original
+
     let attachment = new Trix.Attachment({
       content: mention.content,
       sgid: mention.sgid,
