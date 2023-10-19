b5444647f81221e674fae20f7c3677754df35c96
Rinas <5433320+onerinas@users.noreply.github.com>
2023-10-19 17:55:03 +0400

    Fix: Tooltip controller example usage (#792)
    * Update tooltip controller usage
    
    * Update tooltip_controller.js
    
    * Update tooltip_controller.js
    
    ---------
    
    Co-authored-by: Chris Oliver <excid3@gmail.com>

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,4 +1,9 @@
 // Example usage:
+// <div data-controller="tooltip" data-tooltip-content-value="Hello world"></div>
+//
+// Or using Tippy data attributes
+// See: https://atomiks.github.io/tippyjs/v6/constructor/#attribute
+//      https://atomiks.github.io/tippyjs/v6/customization/
 // <div data-controller="tooltip" data-tippy-content="Hello world"></div>
 
 import { Controller } from "@hotwired/stimulus"
